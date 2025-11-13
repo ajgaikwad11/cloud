c584d796afae01c152cab9d3925af3b23f090e45
Ajit Gaikwad <105539206+ajgaikwad11@users.noreply.github.com>
2025-11-13 23:36:49 +0530

    Create Demo.php

---
+++ Demo.php
@@ -0,0 +1,48 @@
+🟦 WINDOWS (C:\php_app_lab2)
+1. Create folder
+mkdir C:\php_app_lab2
+cd C:\php_app_lab2
+
+2. Create index.php
+notepad index.php
+
+
+Paste:
+
+<?php
+echo "PHP App Running!";
+?>
+
+3. Run with PHP built-in server
+php -S localhost:8000
+
+
+Browser:
+
+http://localhost:8000
+
+
+Screenshot.
+
+🟩 LINUX (/home/student/php_app_lab2/)
+1. Copy folder
+
+To:
+
+/home/student/php_app_lab2/
+
+2. Install PHP
+sudo apt update
+sudo apt install php -y
+
+3. Run app
+cd /home/student/php_app_lab2
+php -S 0.0.0.0:8000
+
+
+Browser:
+
+http://localhost:8000
+
+
+Screenshot.
